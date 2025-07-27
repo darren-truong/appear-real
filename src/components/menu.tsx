2d27435d98d8bcce80fdf8bc99e7d7d8732cc5e2
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-07-26 20:07:19 -0700

    User button & sign out

---
+++ src/components/menu.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ShoppingCart } from "lucide-react";
+import UserButton from "./user-button";
+import { ModeToggle } from "./ui/mode-toggle";
+
+export default function Menu() {
+  return (
+    <div>
+      <nav className="flex w-full max-w-xs gap-1">
+        <ModeToggle />
+        <Button asChild variant="ghost">
+          <Link href="/cart">
+            <ShoppingCart /> Cart
+          </Link>
+        </Button>
+        <UserButton />
+      </nav>
+    </div>
+  );
+}
